945037e92f148a22776f72b01047033c5577354d
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2019-08-16 15:25:41 +0000

    Start migration to react-redux-real-world.
    Migrate authentication and user info.

---
+++ react-redux-front/src/components/Home/index.js
@@ -0,0 +1,58 @@
+import Banner from './Banner';
+import MainView from './MainView';
+import React from 'react';
+import agent from '../../agent';
+import { connect } from 'react-redux';
+import {
+  HOME_PAGE_LOADED,
+  HOME_PAGE_UNLOADED,
+  APPLY_TAG_FILTER
+} from '../../constants/actionTypes';
+
+const Promise = global.Promise;
+
+const mapStateToProps = state => ({
+  ...state.home,
+  appName: state.common.appName,
+  token: state.common.token
+});
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: (tab, pager, payload) =>
+    dispatch({ type: HOME_PAGE_LOADED, tab, pager, payload }),
+  onUnload: () =>
+    dispatch({  type: HOME_PAGE_UNLOADED })
+});
+
+class Home extends React.Component {
+  componentWillMount() {
+    const tab = this.props.token ? 'feed' : 'all';
+    const articlesPromise = this.props.token ?
+      agent.Articles.feed :
+      agent.Articles.all;
+
+    this.props.onLoad(tab, articlesPromise, Promise.all([articlesPromise()]));
+  }
+
+  componentWillUnmount() {
+    this.props.onUnload();
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+
+        <Banner token={this.props.token} appName={this.props.appName} />
+
+        <div className="container page">
+          <div className="row">
+            <MainView />
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ react-redux-front/src/reducers/editor.js
@@ -0,0 +1,39 @@
+import {
+  EDITOR_PAGE_LOADED,
+  EDITOR_PAGE_UNLOADED,
+  ARTICLE_SUBMITTED,
+  ASYNC_START,
+  UPDATE_FIELD_EDITOR
+} from '../constants/actionTypes';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case EDITOR_PAGE_LOADED:
+      return {
+        ...state,
+        articleSlug: action.payload ? action.payload.article.slug : '',
+        title: action.payload ? action.payload.article.title : '',
+        description: action.payload ? action.payload.article.description : '',
+        body: action.payload ? action.payload.article.body : ''
+      };
+    case EDITOR_PAGE_UNLOADED:
+      return {};
+    case ARTICLE_SUBMITTED:
+      return {
+        ...state,
+        inProgress: null,
+        errors: action.error ? action.payload.errors : null
+      };
+    case ASYNC_START:
+      if (action.subtype === ARTICLE_SUBMITTED) {
+        return { ...state, inProgress: true };
+      }
+      break;
+    case UPDATE_FIELD_EDITOR:
+      return { ...state, [action.key]: action.value };
+    default:
+      return state;
+  }
+
+  return state;
+};
